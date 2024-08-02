a6c195b0c1d053007c422657e43479cdcc03f9db
Stanislav Poliakov <polyakov.s.de@sberbank.ru>
2024-08-02 12:06:23 +0300

    Initial Commit

---
+++ otus_homework_20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  otus_homework_20
+//
+//  Created by Поляков Станислав Денисович on 02.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
